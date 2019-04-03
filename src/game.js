a0dc11952703ac8e14c484d6ac6e85711def4ac5
Tom Pearson <tom.g.pearson@gmail.com>
2019-04-03 09:15:55 +0100

    add scores

--- src/game.js
+++ src/game.js
@@ -1,5 +1,20 @@
 const Hand = require('pokersolver').Hand;
 
+const handScore = {
+  'High Card': { points:0, cards:0 },
+  'Pair': { cards:1, points:100 },
+  'Two Pair': { cards:2, points:200 },
+  'Three of a Kind': { cards:3, points:300 },
+  'Straight': { cards:4, points:400 },
+  'Flush': { cards:5, points:500 },
+  'Full House': { cards:6, points:600 },
+  'Four of a Kind': { cards:8, points:800 },
+  'Straight Flush': { cards:12, points:1200 },
+  'Royal Flush': { cards:16, points:1600 },
+  'First of type': { points:500, cards:5 }
+};
+
+
 function fullDeck(){
   return ['hearts','diamonds','spades','clubs'].reduce((deck, suit)=>{
     for(let i=1; i<=13;i ++){
